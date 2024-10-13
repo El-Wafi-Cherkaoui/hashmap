1741c13ef90ac3b1f167fc82de2d172d7a78f2bf
Cherkaoui El Wafi <elwafi.cherkaoui@gmail.com>
2024-10-13 12:46:36 +0100

    Complete second half of assginment

--- hashing.js
+++ hashing.js
@@ -1,13 +1,22 @@
 class HashMap{
-    static hashmap_size = 16
-    static load_factor = 0.75
-
-    static hashmap = new Array(this.hashmap_size)
-    .fill('')
-    .map(()=>{
-        return []
-    })
-    static hash(key) {
+    // static hashmap_size = 16
+    // static load_factor = 0.75
+    constructor(){
+        this.hashmap_size = 16
+        this.load_factor = 0.75
+        this.hashmap = new Array(this.hashmap_size)
+        .fill('')
+        .map(()=>{
+            return []
+        })
+        
+    }
+    // static hashmap = new Array(this.hashmap_size)
+    // .fill('')
+    // .map(()=>{
+    //     return []
+    // })
+    hash(key) {
         let hash_key = 0
         let random_c = 15165165165
         
@@ -19,11 +28,11 @@ class HashMap{
         }
         return hash_key   
     }
-    static set(key, value){
+    set(key, value){
         this.grow_hashmap_size()
-        let hashed_key = this.hash(key)%16   
+        let hashed_key = this.hash(key)%this.hashmap_size   
         
-        let current_bucket = HashMap.hashmap[hashed_key]     
+        let current_bucket = this.hashmap[hashed_key]     
         
         if(current_bucket.length == 0){
             current_bucket.push({key: key, value: value})
@@ -50,7 +59,7 @@ class HashMap{
         
         
     }
-    static get(key){
+    get(key){
         let found = false
         let found_data = ''
 
@@ -67,7 +76,7 @@ class HashMap{
         if(found) return found_data
         else return null
     }
-    static has(key){
+    has(key){
         let found = false
 
         let hashed_key = this.hash(key)%16
@@ -81,10 +90,58 @@ class HashMap{
         })
         return found
     }
-    static optimal_entries(){
+    remove(key){
+        if(this.has(key)){
+            
+            let current_bucket = this.hashmap[this.hash(key)%16] 
+            let value = this.get(key)
+            
+            HashMap.hashmap[this.hash(key)%16] = current_bucket.filter((pair)=>{
+                return pair.key != key && pair.value != value
+            })
+
+            
+        }
+        else{
+            return false
+        }
+    }
+    length(){
+        let counter = 0
+        this.hashmap.forEach((bucket)=>{
+            bucket.forEach((pair)=>{
+                counter += 1
+            })
+        })
+        return counter
+    }
+    clear(){
+        this.hashmap = this.hashmap.map(()=>[])
+    }
+    keys(){
+
+        let all_keys = []
+        this.hashmap.map((bucket)=>{
+            bucket.map((pair)=>{
+                all_keys.push(pair.key)
+            })
+        })
+        return all_keys
+    }
+    values(){
+
+        let all_values = []
+        this.hashmap.map((bucket)=>{
+            bucket.map((pair)=>{
+                all_values.push(pair.value)
+            })
+        })
+        return all_values
+    }
+    optimal_entries(){
         return this.hashmap_size * this.load_factor
     }
-    static total_entries(){
+    total_entries(){
         let total_capacity = 0
         this.hashmap.forEach((bucket)=>{
             bucket.forEach(()=>{
@@ -92,12 +149,25 @@ class HashMap{
             })
         })
         return total_capacity
-        // return this.hashmap_size * this.load_factor
     }
-    static grow_hashmap_size(){        
+    grow_hashmap_size(){        
         if(this.total_entries() >= this.optimal_entries()){
             this.hashmap.push([])
-            this.hashmap_size += 4
+            this.hashmap_size *= 2
+
+            let old_hashmap = this.hashmap
+            this.hashmap = new Array(this.hashmap_size)
+            .fill('')
+            .map(()=>{
+                return []
+            })
+
+            old_hashmap.map((bucket)=>{
+                bucket.map((pair)=>{
+                    this.set(pair.key, pair.value)
+                })
+            })
+
         }
     }
 
@@ -108,26 +178,33 @@ class HashMap{
 
 // console.log(1424720551%16);
 
-HashMap.set('elwafi', '1')
-// HashMap.set('xa', '2')
-HashMap.set('elwafi', '3')
-HashMap.set('xa', '3')
-HashMap.set('as43sadx', '3')
-HashMap.set('xas231h', '3')
-HashMap.set('lkjhjghgf', '3')
-HashMap.set('sda2123', '3')
-HashMap.set('fasfd', '3')
-HashMap.set(' csxsd', '3')
-HashMap.set('kjljkl', '3')
-HashMap.set('mmn65', '3')
-HashMap.set('axxza1123', '3')
-HashMap.set('axsdzad', '3')
-HashMap.set(' a', '3')
-HashMap.set('cxvgfh3', '3')
-HashMap.set(',m,.as asd', '3')
+// HashMap.set('elwafi', 'as')
+// // HashMap.set('xa', '2')
+// HashMap.set('elwafi', '32')
+// HashMap.set('xa', '3')
+// HashMap.set('as43sadx', '213as')
+// HashMap.set('xas231h', 'xxasd')
+// HashMap.set('lkjhjghgf', '3')
+// HashMap.set('sda2123', '3')
+// HashMap.set('fasfd', '3')
+// HashMap.set(' csxsd', '3')
+// HashMap.set('kjljkl', '3')
+// HashMap.set('mmn65', '3')
+// HashMap.set('axxza1123', '3')
+// HashMap.set('axsdzad', '3')
+// HashMap.set(' a', '3')
+// HashMap.set('cxvgfh3', '3')
+// HashMap.set(',m,.as asd', '3')
 
-console.log(HashMap.get('elwafi'))
-console.log(HashMap.has('elwaffdi'))
+// HashMap.remove('xas231h')
+// console.log(HashMap.hashmap);
+// HashMap.clear()
+// console.log(HashMap.hashmap);
+// console.log(HashMap.keys());
+// console.log(HashMap.values());
+// console.log(HashMap.length())
+// console.log(HashMap.get('elwafi'))
+// console.log(HashMap.has('elwaffdi'))
 
 // HashMap.hashmap
 // HashMap.hashmap[0] = ['a']
@@ -140,4 +217,46 @@ console.log(HashMap.has('elwaffdi'))
 // console.log(g);
 // let f = new Array(11).fill([])
 // console.log(1441414%16);
+
+const test = new HashMap()
+
+test.set('apple', 'red')
+test.set('banana', 'yellow')
+test.set('carrot', 'orange')
+test.set('dog', 'brown')
+test.set('elephant', 'gray')
+test.set('frog', 'green')
+test.set('grape', 'purple')
+test.set('hat', 'black')
+test.set('ice cream', 'white')
+test.set('jacket', 'blue')
+test.set('kite', 'pink')
+test.set('lion', 'golden')
+
+
+// overwrite test
+// test.set('kite', 'new kite')
+// test.set('lion', 'sxaxsx')
+// console.log(test.get('kite'))
+
+// test expanding bucktes
+test.set('moon', 'silver')
+
+// test overwriting after expanding
+console.log(test.keys());
+test.set('moon', 'elwafi')
+console.log(test.keys());
+
+// test other methods : 
+console.log(test.get('moon'));
+test.clear();
+
+
+// console.log(test.hashmap_size)
+
+console.log(test.total_entries());
+console.log(test.optimal_entries());
+
+// test.grow_hashmap_size();
+// console.log(test);
 
